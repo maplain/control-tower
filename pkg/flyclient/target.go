e7985d8b03bb3889b16c35ac175676091b704bcc
maplain <fangyuanl@vmware.com>
2018-12-31 19:06:27 -0800

    remove unused structs

--- pkg/flyclient/target.go
+++ pkg/flyclient/target.go
@@ -2,16 +2,4 @@ package client
 
 import "github.com/concourse/fly/rc"
 
-type Target struct {
-	API      string `yaml:"api"`
-	Team     string `yaml:"team"`
-	Insecure string `yaml:"insecure"`
-	Token    Token  `yaml:"token"`
-}
-
-type Token struct {
-	Type  string `yaml:"type"`
-	Value string `yaml:"value"`
-}
-
 type Targets map[rc.TargetName]rc.TargetProps
