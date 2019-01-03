880efc16651e7c4bd06689dea21dadf193948e9c
maplain <fangyuanl@vmware.com>
2019-01-02 20:13:26 -0800

    add noninteractive login
    and automatically fly sync if needed

--- pkg/flyclient/client.go
+++ pkg/flyclient/client.go
@@ -1,14 +1,18 @@
 package client
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/concourse/fly/rc"
 	cterror "github.com/maplain/control-tower/pkg/error"
-	"github.com/maplain/control-tower/pkg/io"
+	cio "github.com/maplain/control-tower/pkg/io"
+	"github.com/skratchdot/open-golang/open"
 )
 
 const (
@@ -23,6 +27,10 @@ type Cmd struct {
 	target rc.TargetName
 	subcmd string
 	args   []string
+	flags  []string
+	stdin  io.Reader
+	stdout io.Writer
+	stderr io.Writer
 }
 
 type Client struct {
@@ -31,8 +39,12 @@ type Client struct {
 }
 
 func NewFlyCmd() *Cmd {
-	res := &Cmd{}
-	path, err := io.BinaryPath(flyBinaryName)
+	res := &Cmd{
+		stdin:  os.Stdin,
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+	}
+	path, err := cio.BinaryPath(flyBinaryName)
 	if err != nil {
 		cterror.Check(errors.New(fmt.Sprintf("%s doesn't exist on your $PATH. install it before your use", flyBinaryName)))
 	}
@@ -55,7 +67,26 @@ func (c *Cmd) WithArg(arg, value string) *Cmd {
 	return c
 }
 
-func (c *Cmd) Run() error {
+func (c *Cmd) WithFlag(flag string) *Cmd {
+	c.flags = append(c.flags, flag)
+	return c
+}
+func (c *Cmd) WithStdin(reader io.Reader) *Cmd {
+	c.stdin = reader
+	return c
+}
+
+func (c *Cmd) WithStdout(writer io.Writer) *Cmd {
+	c.stdout = writer
+	return c
+}
+
+func (c *Cmd) WithStderr(writer io.Writer) *Cmd {
+	c.stderr = writer
+	return c
+}
+
+func (c *Cmd) Cmd() *exec.Cmd {
 	args := []string{}
 	if c.target != "" {
 		args = append(args, "--target", string(c.target))
@@ -63,16 +94,18 @@ func (c *Cmd) Run() error {
 	if c.subcmd != "" {
 		args = append(args, c.subcmd)
 	}
+	args = append(args, c.flags...)
 	args = append(args, c.args...)
 	cmd := exec.Command(flyBinaryName, args...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdin
-	cmd.Stderr = os.Stderr
-	err := cmd.Start()
-	if err != nil {
-		return err
-	}
-	err = cmd.Wait()
+	return cmd
+}
+
+func (c *Cmd) Run() error {
+	cmd := c.Cmd()
+	cmd.Stdin = c.stdin
+	cmd.Stdout = c.stdout
+	cmd.Stderr = c.stderr
+	err := cmd.Run()
 	if err != nil {
 		return err
 	}
@@ -81,7 +114,7 @@ func (c *Cmd) Run() error {
 
 func NewFlyClient() (*Client, error) {
 	res := &Client{}
-	path, err := io.BinaryPath(flyBinaryName)
+	path, err := cio.BinaryPath(flyBinaryName)
 	if err != nil {
 		return res, errors.New(fmt.Sprintf("%s doesn't exist on your $PATH. install it before your use", flyBinaryName))
 	}
@@ -122,4 +155,93 @@ func (c *Client) Targets() []rc.TargetName {
 		names = append(names, n)
 	}
 	return names
+}
+
+func SimpleLogin(target string) error {
+	return NewFlyCmd().
+		WithTarget(rc.TargetName(target)).
+		WithSubCommand("login").
+		WithFlag("-b").
+		Run()
+}
+
+func Login(target string) error {
+	flycmd := NewFlyCmd().
+		WithTarget(rc.TargetName(target)).
+		WithSubCommand("login").
+		Cmd()
+
+	stdout, err := flycmd.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	stderr, err := flycmd.StderrPipe()
+	if err != nil {
+		return err
+	}
+	flycmd.Stdin = os.Stdin
+
+	err = flycmd.Start()
+	if err != nil {
+		return err
+	}
+
+	errc := make(chan error)
+	// stdout will have the webhook
+	go func() {
+		stdoutReader := bufio.NewReader(stdout)
+		line, err := stdoutReader.ReadString('\n')
+		for err == nil {
+			line = strings.TrimSpace(line)
+			if strings.Contains(line, "https://") {
+				errc <- open.Run(line)
+				return
+			}
+			line, err = stdoutReader.ReadString('\n')
+		}
+		if err == io.EOF {
+			errc <- nil
+		} else {
+			errc <- err
+		}
+		return
+	}()
+	// stderr will have the warning for sync, if there is any
+	go func() {
+		stderrReader := bufio.NewReader(stderr)
+		line, err := stderrReader.ReadString('\n')
+		for err == nil {
+			line = strings.TrimSpace(line)
+			if strings.Contains(line, "out of sync") {
+				errc <- NewFlyCmd().
+					WithTarget(rc.TargetName(target)).
+					WithSubCommand("sync").
+					Run()
+				return
+			}
+			line, err = stderrReader.ReadString('\n')
+		}
+		if err == io.EOF {
+			errc <- nil
+		} else {
+			errc <- err
+		}
+		return
+	}()
+	var res string
+	for i := 0; i < 2; i++ {
+		err = <-errc
+		if err != nil {
+			res = res + err.Error()
+		}
+	}
+	close(errc)
+	err = flycmd.Wait()
+	if err != nil {
+		res = res + err.Error()
+	}
+	if res == "" {
+		return nil
+	}
+	return errors.New(res)
 }
