538fb837934819e263a142de19a780e074068378
maplain <fangyuanl@vmware.com>
2019-01-15 12:20:16 -0800

    add support for unpause

---
+++ cmd/fly_unpause.go
@@ -0,0 +1,68 @@
+// Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/concourse/fly/rc"
+	"github.com/maplain/control-tower/pkg/concourseclient"
+	"github.com/maplain/control-tower/pkg/config"
+	cterror "github.com/maplain/control-tower/pkg/error"
+	"github.com/spf13/cobra"
+)
+
+var (
+	flyUnpauseCmdUnpause bool
+)
+
+var flyUnpauseCmd = &cobra.Command{
+	Use:   "p",
+	Short: "if to pause in use context's pipeline",
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, c, err := config.LoadInUseContext()
+		cterror.Check(err)
+
+		inusectx := ctx.Contexts[c]
+
+		cli, err := concourseclient.NewConcourseClient(rc.TargetName(inusectx.Target))
+		cterror.Check(err)
+
+		if flyUnpauseCmdUnpause {
+			unpaused, err := cli.Team().UnpausePipeline(inusectx.Pipeline)
+			cterror.Check(err)
+
+			if unpaused {
+				fmt.Printf("pipeline %s is unpaused.", inusectx.Pipeline)
+			} else {
+				fmt.Printf("pipeline %s is already unpaused.", inusectx.Pipeline)
+			}
+		} else {
+			paused, err := cli.Team().PausePipeline(inusectx.Pipeline)
+			cterror.Check(err)
+
+			if paused {
+				fmt.Printf("pipeline %s is paused.", inusectx.Pipeline)
+			} else {
+				fmt.Printf("pipeline %s is already paused.", inusectx.Pipeline)
+			}
+		}
+	},
+}
+
+func init() {
+	flyCmd.AddCommand(flyUnpauseCmd)
+	flyUnpauseCmd.Flags().BoolVarP(&flyUnpauseCmdUnpause, "unpause", "u", false, "unpause the pipeline")
+}
